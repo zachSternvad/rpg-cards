284563bb50eeebb9f6d2391aea31c04e395b6d8d
zachSternvad <zachery.meck@gmail.com>
2026-09-18 13:24:41 +0200

    Klar med uppgift B: VILKOR MED IF

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,26 @@ function calculatePower(level, hp) {
 // 1. KORT-MALLEN (Komponenten)
 function CharacterCard(props) {
   const hero = props.character;
+  // Dörrvakten
+  if (hero.hp <= 0) {
+    return (
+      <article className="card fallen">
+        <h2>{hero.name}</h2>
+        <p>FALLEN I STRID</p>
+      </article>
+    );
+  }
+
+  let veteranText = "";
+  if (hero.level >= 10) {
+    veteranText = " ★ Veteran";
+  }
+
   const power = calculatePower(hero.level, hero.hp);
 
   return (
     <article className="card">
-      <h2>{hero.name}</h2>
+      <h2>{hero.name}{veteranText}</h2>
       <p>Roll: {hero.role}</p>
       <p>HP: {hero.hp}</p>
       <p>Level: {hero.level}</p>
